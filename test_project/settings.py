6111ec73d99d8c9cbfaf3fa6625ac8bd5d3925da
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2019-04-12 23:12:09 +0200

    Doesn't work with Django < 2

--- test_project/settings.py
+++ test_project/settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 'django_migrations_ignore_attrs',
+    'django_migrations_ignore_attrs',
 
     'test_project',
 )
